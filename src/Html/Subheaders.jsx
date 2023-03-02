56d1a7082d2833f8b731d6c5ba96bfa711fc8c7b
Vi Nguyen <vinguyen1172000@gmail.com>
2023-03-01 23:59:57 -0800

    adjust subheaders

--- src/Html/Subheaders.jsx
+++ src/Html/Subheaders.jsx
@@ -3,26 +3,43 @@ export default () => {
     <>
       <div
         className="subheaders"
-        style={{ position: "absolute", left: "150vw", top: "10vh" }}
+        style={{ position: "absolute", left: "105vw", top: "20vh" }}
+      >
+        Nice to meet you!
+      </div>
+      <div
+        className="subheaders"
+        style={{
+          position: "absolute",
+          left: "150vw",
+          top: "10vh",
+          backgroundColor: "#dad7b4de",
+          color: "white",
+          borderColor: "white",
+        }}
       >
         {`Discover some interesting facts about myself via clickable items >.-`}
       </div>
       <div
         className="subheaders"
-        style={{ position: "absolute", left: "105vw", top: "20vh" }}
-      >
-        Hi! Nice to meet you!
-      </div>
-
-      <div
-        className="subheaders"
-        style={{ position: "absolute", left: "230vw", top: "19vh" }}
+        style={{
+          position: "absolute",
+          left: "230vw",
+          top: "19vh",
+        }}
       >
         Welcome to my messy kitchen :{")"}
       </div>
       <div
         className="subheaders"
-        style={{ position: "absolute", left: "290vw", top: "50vh" }}
+        style={{
+          position: "absolute",
+          left: "290vw",
+          top: "50vh",
+          backgroundColor: "#dad7b4de",
+          color: "white",
+          borderColor: "white",
+        }}
       >
         Some of my works made in Blender
       </div>
