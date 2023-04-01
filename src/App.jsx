15be64cebeed766c9b02d15ab80cad2b95dbf2f7
Vi Nguyen <vinguyen1172000@gmail.com>
2023-04-01 09:54:55 -0700

    update bg selectors

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
   Gallery,
 } from "./Models";
 import Card from "./Models/Helper/Card";
-
+import { bg1, bg2, bg3, bg4, bg5 } from "./-lib/bacgrounds";
 const barStyles = {
   height: "10px",
   borderRadius: "7px",
@@ -30,75 +30,6 @@ const dataStyles = {
   fontSize: "1rem",
   fontFamily: '"Indie Flower", cursive',
 };
-const bg2 = `linear-gradient(
-    180deg,
-    hsl(86deg 41% 71%) 0%,
-    hsl(75deg 40% 71%) 21%,
-    hsl(66deg 38% 71%) 30%,
-    hsl(57deg 39% 73%) 39%,
-    hsl(49deg 44% 75%) 46%,
-    hsl(43deg 48% 77%) 54%,
-    hsl(38deg 52% 79%) 61%,
-    hsl(34deg 54% 80%) 69%,
-    hsl(30deg 54% 82%) 79%,
-    hsl(26deg 52% 84%) 100%
-    )`;
-const bg1 = `linear-gradient(
-      180deg,
-      hsl(216deg 27% 61%) 0%,
-      hsl(208deg 33% 62%) 21%,
-      hsl(201deg 38% 63%) 30%,
-      hsl(195deg 41% 65%) 39%,
-      hsl(189deg 41% 66%) 46%,
-      hsl(182deg 39% 67%) 54%,
-      hsl(174deg 41% 71%) 61%,
-      hsl(163deg 44% 75%) 69%,
-      hsl(149deg 47% 80%) 79%,
-      hsl(128deg 49% 85%) 100%
-    )`;
-
-const bg5 = `linear-gradient(
-  180deg,
-  hsl(81deg 27% 55%) 0%,
-  hsl(81deg 27% 60%) 11%,
-  hsl(80deg 27% 65%) 22%,
-  hsl(80deg 27% 70%) 33%,
-  hsl(80deg 27% 74%) 44%,
-  hsl(79deg 27% 79%) 56%,
-  hsl(79deg 28% 85%) 67%,
-  hsl(79deg 28% 90%) 78%,
-  hsl(78deg 28% 95%) 89%,
-  hsl(0deg 0% 100%) 100%
-    
-    )`;
-
-const bg3 = `linear-gradient(
-      180deg,
-      #7289ac 0%,
-      hsl(234deg 21% 62%) 9%,
-      hsl(263deg 19% 65%) 22%,
-      hsl(296deg 17% 67%) 40%,
-      hsl(325deg 25% 71%) 58%,
-      hsl(342deg 35% 76%) 72%,
-      hsl(356deg 44% 81%) 82%,
-      hsl(9deg 55% 83%) 90%,
-      hsl(18deg 65% 85%) 96%,
-      #f6dbc6 100%
-    )`;
-
-const bg4 = `linear-gradient(
-   180deg,
-      #62799d 0%,
-      hsl(218deg 23% 56%) 24%,
-      hsl(219deg 23% 61%) 38%,
-      hsl(219deg 23% 67%) 49%,
-      hsl(220deg 23% 72%) 59%,
-      hsl(220deg 23% 77%) 68%,
-      hsl(221deg 23% 83%) 76%,
-      hsl(221deg 23% 89%) 85%,
-      hsl(221deg 23% 94%) 92%,
-      hsl(0deg 0% 100%) 100%
-)`;
 
 const containerStyles = {
   background: bg5,
