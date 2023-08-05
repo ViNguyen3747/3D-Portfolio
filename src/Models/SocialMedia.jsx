40d94b2fc5506578805e8a232dc46dc02b108590
Vi Nguyen <vinguyen1172000@gmail.com>
2023-08-05 12:53:20 -0700

    update social media

--- src/Models/SocialMedia.jsx
+++ src/Models/SocialMedia.jsx
@@ -4,7 +4,6 @@ import { useThree } from "@react-three/fiber";
 const LinkMapper = {
   be: "https://www.behance.net/vinguyen100",
   email: "mailto:vinguyen1172000@gmail.com",
-  facebook: "https://www.facebook.com/profile.php?id=100010227366087",
   github: "https://github.com/ViNguyen3747",
   insta: "https://www.instagram.com/v_nguyen_711/",
   linkedin: "https://www.linkedin.com/in/thi-kieu-vi-nguyen-0894a919a/",
@@ -46,7 +45,6 @@ export default () => {
     <Center position={[w * 4, 0, 0]}>
       <SocialMediaLink obj={nodes.be} label={"be"} w={w} h={h} />
       <SocialMediaLink obj={nodes.email} label={"email"} w={w} h={h} />
-      <SocialMediaLink obj={nodes.facebook} label={"facebook"} w={w} h={h} />
       <SocialMediaLink obj={nodes.github} label={"github"} w={w} h={h} />
       <SocialMediaLink obj={nodes.insta} label={"insta"} w={w} h={h} />
       <SocialMediaLink obj={nodes.linkdin} label={"linkedin"} w={w} h={h} />
