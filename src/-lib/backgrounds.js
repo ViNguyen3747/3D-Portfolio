ce3840670b32a29c2c1338da5ce519e5123c950d
Vi Nguyen <vinguyen1172000@gmail.com>
2023-08-05 19:11:43 -0700

    align codes

--- src/-lib/backgrounds.js
+++ src/-lib/backgrounds.js
@@ -1,29 +1,29 @@
 const bg2 = `linear-gradient(
-    180deg,
-    hsl(86deg 41% 71%) 0%,
-    hsl(75deg 40% 71%) 21%,
-    hsl(66deg 38% 71%) 30%,
-    hsl(57deg 39% 73%) 39%,
-    hsl(49deg 44% 75%) 46%,
-    hsl(43deg 48% 77%) 54%,
-    hsl(38deg 52% 79%) 61%,
-    hsl(34deg 54% 80%) 69%,
-    hsl(30deg 54% 82%) 79%,
-    hsl(26deg 52% 84%) 100%
-    )`;
+  180deg,
+  hsl(86deg 41% 71%) 0%,
+  hsl(75deg 40% 71%) 21%,
+  hsl(66deg 38% 71%) 30%,
+  hsl(57deg 39% 73%) 39%,
+  hsl(49deg 44% 75%) 46%,
+  hsl(43deg 48% 77%) 54%,
+  hsl(38deg 52% 79%) 61%,
+  hsl(34deg 54% 80%) 69%,
+  hsl(30deg 54% 82%) 79%,
+  hsl(26deg 52% 84%) 100%
+)`;
 const bg1 = `linear-gradient(
-      180deg,
-      hsl(216deg 27% 61%) 0%,
-      hsl(208deg 33% 62%) 21%,
-      hsl(201deg 38% 63%) 30%,
-      hsl(195deg 41% 65%) 39%,
-      hsl(189deg 41% 66%) 46%,
-      hsl(182deg 39% 67%) 54%,
-      hsl(174deg 41% 71%) 61%,
-      hsl(163deg 44% 75%) 69%,
-      hsl(149deg 47% 80%) 79%,
-      hsl(128deg 49% 85%) 100%
-    )`;
+  180deg,
+  hsl(216deg 27% 61%) 0%,
+  hsl(208deg 33% 62%) 21%,
+  hsl(201deg 38% 63%) 30%,
+  hsl(195deg 41% 65%) 39%,
+  hsl(189deg 41% 66%) 46%,
+  hsl(182deg 39% 67%) 54%,
+  hsl(174deg 41% 71%) 61%,
+  hsl(163deg 44% 75%) 69%,
+  hsl(149deg 47% 80%) 79%,
+  hsl(128deg 49% 85%) 100%
+)`;
 
 const bg5 = `linear-gradient(
   180deg,
@@ -37,34 +37,34 @@ const bg5 = `linear-gradient(
   hsl(79deg 28% 90%) 78%,
   hsl(78deg 28% 95%) 89%,
   hsl(0deg 0% 100%) 100%
-    )`;
+)`;
 
 const bg3 = `linear-gradient(
-      180deg,
-      #7289ac 0%,
-      hsl(234deg 21% 62%) 9%,
-      hsl(263deg 19% 65%) 22%,
-      hsl(296deg 17% 67%) 40%,
-      hsl(325deg 25% 71%) 58%,
-      hsl(342deg 35% 76%) 72%,
-      hsl(356deg 44% 81%) 82%,
-      hsl(9deg 55% 83%) 90%,
-      hsl(18deg 65% 85%) 96%,
-      #f6dbc6 100%
-    )`;
+  180deg,
+  #7289ac 0%,
+  hsl(234deg 21% 62%) 9%,
+  hsl(263deg 19% 65%) 22%,
+  hsl(296deg 17% 67%) 40%,
+  hsl(325deg 25% 71%) 58%,
+  hsl(342deg 35% 76%) 72%,
+  hsl(356deg 44% 81%) 82%,
+  hsl(9deg 55% 83%) 90%,
+  hsl(18deg 65% 85%) 96%,
+  #f6dbc6 100%
+)`;
 
 const bg4 = `linear-gradient(
-   180deg,
-      #62799d 0%,
-      hsl(218deg 23% 56%) 24%,
-      hsl(219deg 23% 61%) 38%,
-      hsl(219deg 23% 67%) 49%,
-      hsl(220deg 23% 72%) 59%,
-      hsl(220deg 23% 77%) 68%,
-      hsl(221deg 23% 83%) 76%,
-      hsl(221deg 23% 89%) 85%,
-      hsl(221deg 23% 94%) 92%,
-      hsl(0deg 0% 100%) 100%
+  180deg,
+  #62799d 0%,
+  hsl(218deg 23% 56%) 24%,
+  hsl(219deg 23% 61%) 38%,
+  hsl(219deg 23% 67%) 49%,
+  hsl(220deg 23% 72%) 59%,
+  hsl(220deg 23% 77%) 68%,
+  hsl(221deg 23% 83%) 76%,
+  hsl(221deg 23% 89%) 85%,
+  hsl(221deg 23% 94%) 92%,
+  hsl(0deg 0% 100%) 100%
 )`;
 
 export { bg1, bg2, bg3, bg4, bg5 };
