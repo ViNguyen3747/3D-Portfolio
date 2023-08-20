b1f3518146e51d1b130039f49fd72dee350a5922
Vi Nguyen <vinguyen1172000@gmail.com>
2023-08-20 10:22:55 -0700

    moves all svg into Icons file

--- src/Html/ProjectBoard.jsx
+++ src/Html/ProjectBoard.jsx
@@ -1,3 +1,4 @@
+import { Sunflower } from "./Icons";
 export default () => {
   return (
     <div id="project-board-container">
@@ -12,7 +13,7 @@ export default () => {
             <div className="description">
               <p>Strawberry</p>
               <p className="description-subtitle">
-                A 2.5D animation inspired by the song "Strawberry" - Epik High
+                An animation inspired by the song "Strawberry" - Epik High
               </p>
             </div>
           </a>
@@ -35,7 +36,7 @@ export default () => {
             <div className="description">
               <p>Car in Mars</p>
               <p className="description-subtitle">
-                A 2.5D animation inspired by the song "Summer in Mars" - keseki
+                An animation inspired by the song "Summer in Mars" - keseki
               </p>
             </div>
           </a>
@@ -80,7 +81,7 @@ export default () => {
             <div className="description">
               <p>SweetVille Express</p>
               <p className="description-subtitle">
-                a 3D world of dessert-inspired buildings, bustling trains, and
+                A 3D world of dessert-inspired buildings, bustling trains, and
                 captivating day-to-night transformations
               </p>
             </div>
@@ -92,7 +93,7 @@ export default () => {
             <div className="description">
               <p>Late-night work...</p>
               <p className="description-subtitle">
-                a 2.5D animation portraying a workaholic's night
+                An animation portraying a workaholic's night
               </p>
             </div>
           </a>
@@ -103,7 +104,7 @@ export default () => {
             <div className="description">
               <p>Lake Twilight Transition</p>
               <p className="description-subtitle">
-                a 2D animation captivating a serene transformation from day to
+                An animation captivating a serene transformation from day to
                 night
               </p>
             </div>
@@ -138,7 +139,7 @@ export default () => {
             <div className="description">
               <p>Sunset</p>
               <p className="description-subtitle">
-                A 2.5D animation capturing the essence of sunset
+                An animation capturing the essence of sunset
               </p>
             </div>
           </a>
@@ -171,8 +172,8 @@ export default () => {
             <div className="description">
               <p>Cosmic Pages</p>
               <p className="description-subtitle">
-                a 2.5D animation unveiling a universe within the pages of a
-                flipping book
+                An animation unveiling a universe within the pages of a flipping
+                book
               </p>
             </div>
           </a>
@@ -244,211 +245,3 @@ export default () => {
     </div>
   );
 };
-
-const Sunflower = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    x="0px"
-    y="0px"
-    width="100"
-    height="100"
-    viewBox="0 0 100 100"
-  >
-    <path
-      d="M52,96.5c-2,0-3.7-0.9-4.9-2.3C34.4,92.7,21,83.6,18.7,69.8c-0.3-2.1,0.3-4.2,1.8-5.7c1.2-1.2,2.9-1.9,4.6-1.9	c0,0,0.1,0,0.1,0c-0.1-0.5-0.2-1-0.2-1.5c0-1,0-1.9,0.1-2.9c-0.9-0.3-1.8-0.6-2.7-1c-3.1-1.4-4.5-4.9-3.3-8.1	c0.3-0.9,0.7-1.7,1.1-2.5c-0.7-0.6-1.4-1.2-2-1.9c-2.3-2.5-2.3-6.3,0-8.8c0.6-0.7,1.4-1.4,2.1-2c-0.5-0.8-0.9-1.7-1.2-2.6	c-1.2-3.2,0.3-6.7,3.4-8.1c0.8-0.4,1.7-0.7,2.6-1C25,21.1,25,20.1,25,19.2c0.1-3.4,2.8-6.1,6.2-6.2c0.3,0,0.5,0,0.8,0	c0.7,0,1.4,0,2.1,0.1c0.3-0.9,0.6-1.8,1-2.7c1.1-2.3,3.4-3.7,5.9-3.7c0.8,0,1.5,0.1,2.3,0.4c0.8,0.3,1.7,0.7,2.5,1.1	c0.6-0.7,1.2-1.4,1.9-2c1.2-1.1,2.8-1.7,4.4-1.7c1.6,0,3.2,0.6,4.4,1.7c0.7,0.7,1.4,1.4,2,2.1c0.8-0.5,1.7-0.9,2.6-1.2	c0.7-0.3,1.4-0.4,2.2-0.4c2.5,0,4.8,1.5,5.9,3.8c0.4,0.8,0.7,1.7,1,2.6c0.7-0.1,1.3-0.1,2-0.1c0.3,0,0.5,0,0.8,0	c3.4,0.2,6.1,2.8,6.2,6.2c0,1,0,1.9-0.1,2.9c0.9,0.3,1.8,0.6,2.7,1c3.1,1.4,4.5,4.9,3.3,8.1c-0.3,0.9-0.7,1.7-1.1,2.5	c0.7,0.6,1.4,1.2,2,1.9c2.3,2.5,2.3,6.3,0,8.8c-0.6,0.7-1.4,1.4-2.1,2c0.5,0.8,0.9,1.7,1.2,2.6c1.2,3.2-0.3,6.7-3.4,8.1	c-0.8,0.4-1.7,0.7-2.6,1C79,59,79,59.9,79,60.8c0,0.5-0.1,1-0.2,1.5c0,0,0.1,0,0.1,0c1.7,0,3.4,0.7,4.6,1.9c1.5,1.5,2.1,3.6,1.8,5.7	C83,83.5,69.6,92.5,57,94.2C55.8,95.6,54,96.5,52,96.5z"
-      opacity=".35"
-    ></path>
-    <path
-      fill="#f2f2f2"
-      d="M50,94.5c-2,0-3.7-0.9-4.9-2.3C32.4,90.7,19,81.6,16.7,67.8c-0.3-2.1,0.3-4.2,1.8-5.7c1.2-1.2,2.9-1.9,4.6-1.9	c0,0,0.1,0,0.1,0c-0.1-0.5-0.2-1-0.2-1.5c0-1,0-1.9,0.1-2.9c-0.9-0.3-1.8-0.6-2.7-1c-3.1-1.4-4.5-4.9-3.3-8.1	c0.3-0.9,0.7-1.7,1.1-2.5c-0.7-0.6-1.4-1.2-2-1.9c-2.3-2.5-2.3-6.3,0-8.8c0.6-0.7,1.4-1.4,2.1-2c-0.5-0.8-0.9-1.7-1.2-2.6	c-1.2-3.2,0.3-6.7,3.4-8.1c0.8-0.4,1.7-0.7,2.6-1C23,19.1,23,18.1,23,17.2c0.1-3.4,2.8-6.1,6.2-6.2c0.3,0,0.5,0,0.8,0	c0.7,0,1.4,0,2.1,0.1c0.3-0.9,0.6-1.8,1-2.7c1.1-2.3,3.4-3.7,5.9-3.7c0.8,0,1.5,0.1,2.3,0.4c0.8,0.3,1.7,0.7,2.5,1.1	c0.6-0.7,1.2-1.4,1.9-2c1.2-1.1,2.8-1.7,4.4-1.7c1.6,0,3.2,0.6,4.4,1.7c0.7,0.7,1.4,1.4,2,2.1c0.8-0.5,1.7-0.9,2.6-1.2	c0.7-0.3,1.4-0.4,2.2-0.4c2.5,0,4.8,1.5,5.9,3.8c0.4,0.8,0.7,1.7,1,2.6c0.7-0.1,1.3-0.1,2-0.1c0.3,0,0.5,0,0.8,0	c3.4,0.2,6.1,2.8,6.2,6.2c0,1,0,1.9-0.1,2.9c0.9,0.3,1.8,0.6,2.7,1c3.1,1.4,4.5,4.9,3.3,8.1c-0.3,0.9-0.7,1.7-1.1,2.5	c0.7,0.6,1.4,1.2,2,1.9c2.3,2.5,2.3,6.3,0,8.8c-0.6,0.7-1.4,1.4-2.1,2c0.5,0.8,0.9,1.7,1.2,2.6c1.2,3.2-0.3,6.7-3.4,8.1	c-0.8,0.4-1.7,0.7-2.6,1C77,57,77,57.9,77,58.8c0,0.5-0.1,1-0.2,1.5c0,0,0.1,0,0.1,0c1.7,0,3.4,0.7,4.6,1.9c1.5,1.5,2.1,3.6,1.8,5.7	C81,81.5,67.6,90.5,55,92.2C53.8,93.6,52,94.5,50,94.5z"
-    ></path>
-    <path
-      fill="#96c362"
-      d="M51.5,86c0,0,1.6-15.3,25.3-19.2C74.9,78.5,61.7,85.6,51.5,86z"
-    ></path>
-    <path
-      fill="#96c362"
-      d="M48.2,86c0,0-1.6-15.3-25.1-19.2C25.1,78.5,38.1,85.6,48.2,86z"
-    ></path>
-    <path
-      fill="#f9b84f"
-      d="M70.4,42.1c3.4-0.3,6.5-1.8,8.6-4.1c-2-2.2-5-3.7-8.3-4.1c3-1.6,5.1-4.1,6.1-6.9c-2.8-1.3-6.2-1.6-9.5-0.5	c2.2-2.6,3.3-5.9,3.2-9c-3-0.1-6.1,0.9-8.8,3c1-3.2,0.7-6.5-0.6-9.2c-2.9,1-5.5,3.3-7.1,6.3c-0.3-3.4-1.8-6.5-4.1-8.6	c-2.2,2-3.7,5-4.1,8.3c-1.6-3-4.1-5.1-6.9-6.1c-1.3,2.8-1.6,6.2-0.5,9.5c-2.6-2.2-5.9-3.3-9-3.2c-0.1,3,0.9,6.1,3,8.8	c-3.2-1-6.5-0.7-9.2,0.6c1,2.9,3.3,5.5,6.3,7.1c-3.4,0.3-6.5,1.8-8.6,4.1c2,2.2,5,3.7,8.3,4.1c-3,1.6-5.1,4.1-6.1,6.9	c2.8,1.3,6.2,1.6,9.5,0.5c-2.2,2.6-3.3,5.9-3.2,9c3,0.1,6.1-0.9,8.8-3c-1,3.2-0.7,6.5,0.6,9.2c2.9-1,5.5-3.3,7.1-6.3	c0.3,3.4,1.8,6.5,4.1,8.6c2.2-2,3.7-5,4.1-8.3c1.6,3,4.1,5.1,6.9,6.1c1.3-2.8,1.6-6.2,0.5-9.5c2.6,2.2,5.9,3.3,9,3.2	c0.1-3-0.9-6.1-3-8.8c3.2,1,6.5,0.7,9.2-0.6C75.7,46.3,73.5,43.7,70.4,42.1z"
-    ></path>
-    <path
-      fill="#739f4d"
-      d="M47.7,85.7C42.6,81,30.6,72.5,22.4,66.7C24.5,78.4,37.7,85.6,47.7,85.7z"
-    ></path>
-    <path
-      fill="#739f4d"
-      d="M51.8,86c5.1-4.8,17.4-13.1,25.5-19.1C75.4,78.6,61.9,85.6,51.8,86z"
-    ></path>
-    <path
-      fill="none"
-      stroke="#40396e"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
-      strokeWidth="3"
-      d="M51.5,86c0,0,1.6-15.3,25.3-19.2C74.9,78.5,61.7,85.6,51.5,86z"
-    ></path>
-    <path
-      fill="none"
-      stroke="#40396e"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
-      strokeWidth="3"
-      d="M48.2,86c0,0-1.6-15.3-25.1-19.2C25.1,78.5,38.1,85.6,48.2,86z"
-    ></path>
-    <ellipse
-      cx="56.3"
-      cy="22.7"
-      fill="#ffc571"
-      rx="10.3"
-      ry="3"
-      transform="rotate(-67.405 56.347 22.748)"
-    ></ellipse>
-    <ellipse
-      cx="43.7"
-      cy="53.2"
-      fill="#ffc571"
-      rx="10.3"
-      ry="3"
-      transform="rotate(-67.405 43.654 53.245)"
-    ></ellipse>
-    <path
-      fill="none"
-      stroke="#40396e"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
-      strokeWidth="3"
-      d="M70.4,42.1c3.4-0.3,6.5-1.8,8.6-4.1c-2-2.2-5-3.7-8.3-4.1c3-1.6,5.1-4.1,6.1-6.9c-2.8-1.3-6.2-1.6-9.5-0.5	c2.2-2.6,3.3-5.9,3.2-9c-3-0.1-6.1,0.9-8.8,3c1-3.2,0.7-6.5-0.6-9.2c-2.9,1-5.5,3.3-7.1,6.3c-0.3-3.4-1.8-6.5-4.1-8.6	c-2.2,2-3.7,5-4.1,8.3c-1.6-3-4.1-5.1-6.9-6.1c-1.3,2.8-1.6,6.2-0.5,9.5c-2.6-2.2-5.9-3.3-9-3.2c-0.1,3,0.9,6.1,3,8.8	c-3.2-1-6.5-0.7-9.2,0.6c1,2.9,3.3,5.5,6.3,7.1c-3.4,0.3-6.5,1.8-8.6,4.1c2,2.2,5,3.7,8.3,4.1c-3,1.6-5.1,4.1-6.1,6.9	c2.8,1.3,6.2,1.6,9.5,0.5c-2.2,2.6-3.3,5.9-3.2,9c3,0.1,6.1-0.9,8.8-3c-1,3.2-0.7,6.5,0.6,9.2c2.9-1,5.5-3.3,7.1-6.3	c0.3,3.4,1.8,6.5,4.1,8.6c2.2-2,3.7-5,4.1-8.3c1.6,3,4.1,5.1,6.9,6.1c1.3-2.8,1.6-6.2,0.5-9.5c2.6,2.2,5.9,3.3,9,3.2	c0.1-3-0.9-6.1-3-8.8c3.2,1,6.5,0.7,9.2-0.6C75.7,46.3,73.5,43.7,70.4,42.1z"
-    ></path>
-    <line
-      x1="50"
-      x2="50"
-      y1="67.2"
-      y2="88"
-      fill="none"
-      stroke="#40396e"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
-      strokeWidth="3"
-    ></line>
-    <ellipse
-      cx="65.3"
-      cy="31.7"
-      fill="#ffc571"
-      rx="10.3"
-      ry="3"
-      transform="rotate(-22.403 65.273 31.702)"
-    ></ellipse>
-    <ellipse
-      cx="34.7"
-      cy="44.3"
-      fill="#ffc571"
-      rx="10.3"
-      ry="3"
-      transform="rotate(-22.403 34.733 44.292)"
-    ></ellipse>
-    <ellipse
-      cx="65.2"
-      cy="44.3"
-      fill="#ffc571"
-      rx="3"
-      ry="10.3"
-      transform="rotate(-67.405 65.249 44.343)"
-    ></ellipse>
-    <ellipse
-      cx="34.8"
-      cy="31.6"
-      fill="#ffc571"
-      rx="3"
-      ry="10.3"
-      transform="rotate(-67.405 34.752 31.65)"
-    ></ellipse>
-    <ellipse
-      cx="56.3"
-      cy="53.3"
-      fill="#ffc571"
-      rx="3"
-      ry="10.3"
-      transform="rotate(-22.403 56.298 53.267)"
-    ></ellipse>
-    <ellipse
-      cx="43.7"
-      cy="22.7"
-      fill="#ffc571"
-      rx="3"
-      ry="10.3"
-      transform="rotate(-22.403 43.708 22.727)"
-    ></ellipse>
-    <ellipse
-      cx="66.5"
-      cy="38"
-      fill="#ffe385"
-      rx="10.3"
-      ry="3"
-      transform="translate(-.06 .106)"
-    ></ellipse>
-    <ellipse
-      cx="33.5"
-      cy="38"
-      fill="#ffe385"
-      rx="10.3"
-      ry="3"
-      transform="translate(-.06 .053)"
-    ></ellipse>
-    <ellipse
-      cx="61.7"
-      cy="49.7"
-      fill="#ffe385"
-      rx="3"
-      ry="10.3"
-      transform="rotate(-45.09 61.7 49.657)"
-    ></ellipse>
-    <ellipse
-      cx="38.3"
-      cy="26.3"
-      fill="#ffe385"
-      rx="3"
-      ry="10.3"
-      transform="rotate(-45.09 38.305 26.336)"
-    ></ellipse>
-    <ellipse
-      cx="50"
-      cy="54.5"
-      fill="#ffe385"
-      rx="3"
-      ry="10.3"
-      transform="translate(-.087 .08)"
-    ></ellipse>
-    <ellipse
-      cx="50"
-      cy="21.5"
-      fill="#ffe385"
-      rx="3"
-      ry="10.3"
-      transform="translate(-.034 .08)"
-    ></ellipse>
-    <ellipse
-      cx="38.3"
-      cy="49.7"
-      fill="#ffe385"
-      rx="10.3"
-      ry="3"
-      transform="rotate(-45.09 38.342 49.694)"
-    ></ellipse>
-    <ellipse
-      cx="61.7"
-      cy="26.3"
-      fill="#ffe385"
-      rx="10.3"
-      ry="3"
-      transform="rotate(-45.09 61.663 26.298)"
-    ></ellipse>
-    <circle cx="50" cy="38" r="13" fill="#40396e"></circle>
-  </svg>
-);
